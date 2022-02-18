bdd93ad922b8e1843ceee8bbe0755e80a17781b3
3nayreddy <91788949+3nayreddy@users.noreply.github.com>
2022-02-18 13:11:31 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hi, Welcome to DevOps and CI CD");
+    response.end("Hi, Welcome to DevOps an CI CD");
 
 });
 
